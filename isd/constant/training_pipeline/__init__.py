bdb8992bbf8a30725e334502fb7f211f9ae19ef3
rian <mdrianurrahaman@gmail.com>
2024-03-28 11:02:42 +0530

    data validation added

--- isd/constant/training_pipeline/__init__.py
+++ isd/constant/training_pipeline/__init__.py
@@ -13,3 +13,14 @@ DATA_INGESTION_S3_DATA_NAME: str = "isd_data_mini.zip"
 DATA_BUCKET_NAME = "isd-object-detection-24"
 
 
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["images", "labels", "classes.names", "train.txt", "val.txt"]
+
+
